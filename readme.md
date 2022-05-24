d12ad0067e6802a16d2711022d927486aadb707c
Heppy <heppy@ekberg.nu>
2022-05-24 12:55:20 +0200

    Initial commit

---
+++ readme.md
@@ -0,0 +1,10 @@
+### Composer:
+Add elicit modal as a depencency with Composer
+```
+  "require": {
+    "heppyekberg/laravelform": "dev-main"
+  },
+  "repositories": [
+    { "type": "vcs", "url": "https://github.com/HeppyEkberg/laravel-form.git" }
+  ],
+```
---
+++ src/ServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace HeppyEkberg\LaravelForm;
+
+use Illuminate\Support\ServiceProvider AS DefaultServiceProvider;
+
+class ServiceProvider extends DefaultServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register() {
+
+    }
+}
